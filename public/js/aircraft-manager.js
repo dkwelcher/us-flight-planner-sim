689e5bb1daae907f09d112f0bb1c9c391d551851
dkwelcher <dkwelcher@gmail.com>
2023-10-18 17:46:39 -0400

    Added aircraft-manager.js & necessary functionality to test server
    Note that validation is simple for testing purposes.

---
+++ public/js/aircraft-manager.js
@@ -0,0 +1,45 @@
+$("#create-button").on("click", function () {
+  const id = $("#create-aircraft-id").val().trim();
+  const name = $("#create-aircraft-name").val().trim();
+  const range = $("#create-aircraft-range").val().trim();
+
+  const errorMessage = validateInput(id, name, range);
+
+  if (errorMessage.length !== 0) {
+    $("#error-msg-create").text(errorMessage).css("visibility", "visible");
+  } else {
+    $("#error-msg-create").css("visibility", "hidden");
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:8888/create-aircraft",
+      data: JSON.stringify({
+        id: id,
+        name: name,
+        range: range,
+      }),
+      contentType: "application/json",
+      success: function (response) {
+        console.log(response.message);
+      },
+      error: function (error) {
+        console.log("Error: " + error);
+      },
+    });
+  }
+});
+
+function validateInput(id, name, range) {
+  if (id.length > 25) {
+    return "ID must be 25 characters or fewer.";
+  } else if (name.length > 50) {
+    return "Name must be 50 characters or fewer.";
+  } else if (range.length > 10 || !isAllDigits(range)) {
+    return "Range must be 10 digits or fewer.";
+  } else {
+    return "";
+  }
+}
+
+function isAllDigits(range) {
+  return /^\d+$/.test(range);
+}
