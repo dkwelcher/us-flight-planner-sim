7c969ab2408aaa2154e8ea9a15d5f7384f254072
dkwelcher <dkwelcher@gmail.com>
2023-10-19 12:22:42 -0400

    Added database connection and modified post request

--- server.js
+++ server.js
@@ -1,19 +1,45 @@
 const express = require("express");
 const cors = require("cors");
+const admin = require("firebase-admin");
+require("dotenv").config();
 
 const app = express();
 const PORT = 8888;
 
 app.use(cors());
-
 app.use(express.json());
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url:
+      process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  }),
+  databaseURL: "https://us-flight-planner-sim-default-rtdb.firebaseio.com/",
+});
+
+const db = admin.database();
 
 app.post("/create-aircraft", (req, res) => {
   const { id, name, range } = req.body;
 
-  // Create airport in database
+  const ref = db.ref(`aircrafts/${id}`);
 
-  res.json({ message: "SUCCESS" });
+  ref.set({ id, name, range }, (error) => {
+    if (error) {
+      res.status(500).json({ message: "FAILURE" });
+    } else {
+      res.json({ message: "SUCCESS" });
+    }
+  });
 });
 
 app.listen(PORT, () => {
