abb17419613191c3742c03da112f4a6901da1f0b
dkwelcher <dkwelcher@gmail.com>
2023-10-25 17:52:29 -0400

    Added Select2 select box functionality

--- public/js/flight-planner.js
+++ public/js/flight-planner.js
@@ -1,4 +1,46 @@
 $(function () {
+  // Aircraft Select2
+  $("#aircraft-name").select2({
+    ajax: {
+      url: "http://localhost:8888/aircrafts",
+      dataType: "json",
+      delay: 250,
+      data: function (params) {
+        return {
+          term: params.term || "",
+        };
+      },
+      processResults: function (data) {
+        return {
+          results: data,
+        };
+      },
+      cache: true,
+    },
+    minimumInputLength: 1,
+  });
+
+  // Airport Select2
+  $("#starting-airport-name, #destination-airport-name").select2({
+    ajax: {
+      url: "http://localhost:8888/airports",
+      dataType: "json",
+      delay: 250,
+      data: function (params) {
+        return {
+          term: params.term || "",
+        };
+      },
+      processResults: function (data) {
+        return {
+          results: data,
+        };
+      },
+      cache: true,
+    },
+    minimumInputLength: 1,
+  });
+
   const mapContainer = $("#planner-map");
 
   if (mapContainer.length) {
